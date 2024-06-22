4bb21fc88881c0f5a6218dd25c2a85f6fe7e0801
Ta1dhen1 <Ta1dhen1@mail.ru>
2024-06-22 13:12:47 +0300

    Completed

--- src/main/java/DataContainer.java
+++ src/main/java/DataContainer.java
@@ -3,21 +3,25 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public  class DataContainer {
+public class DataContainer {
     private String data;
     private int number;
 }
 
 @Inherited_Annotation(dayOfWeek=2)
 abstract class ParentClass {
-    public void someMethod() {}
+    abstract public void someMethod(String data, int num);
+    abstract public DataContainer someMethod2(String data, int num);
 }
 
 class ChildClass1 extends ParentClass{
+    @Override
     @Repeatable_Annotation(description = "Goodbuy", priority = 1, hours = 4)
     public void someMethod(@NonNull String data, @NonNull int num) {
         System.out.println("Hello World!!!");
     }
+    @Repeatable_Annotation(description = "Some text", priority = 52, hours = 3)
+    public DataContainer someMethod2(String data, int num){return new DataContainer(data, num);}
 }
 
 @Inherited_Annotation(dayOfWeek=1)
@@ -26,4 +30,6 @@ class ChildClass2 extends ParentClass{
     public void someMethod(@NonNull String data, @NonNull int num) {
         System.out.println("Hello World!!!");
     }
+    @Repeatable_Annotation(description = "Some text", priority = 999, hours = 11)
+    public DataContainer someMethod2(String data, int num){return new DataContainer(data, num);}
 }
